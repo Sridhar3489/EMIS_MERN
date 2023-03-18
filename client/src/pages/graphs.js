fedafceb7436170a5928ff37bcc4ff441d49eede
Snehitha-23 <katakamsnehitha@gmail.com>
2023-03-18 21:20:39 +0530

    added graph page

--- client/src/pages/graphs.js
+++ client/src/pages/graphs.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import ReactApexChart from 'react-apexcharts';
 const Graphs = () => {
-  return (
+    const color=['#475be8','#e4e8ef'];
+  return (<>
     <div>graphs</div>
-  )
+    
+    <ReactApexChart options={{chart:{type:'donut'},color,legend:{show:false},dataLabels:{enabled:false},}} series={[75,25]} type="donut" width="120px"/>
+ </> )
 }
 
 export default Graphs
